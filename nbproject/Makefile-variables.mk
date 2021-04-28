db9e3e36d3ee9ec90c32d75dd1a23e383ce840ca
sudheer.herle@microchip.com <sudheer.herle@microchip.com>
2021-04-28 07:10:44 +0000

    Project created

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=xpress-atmega324pb.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/xpress-atmega324pb.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=xpress-atmega324pb.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/xpress-atmega324pb.tar
